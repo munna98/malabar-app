7c7f57ef921290866a56e447e057cdc0e22fbfc7
Munna <tmunavir99@gmail.com>
2024-07-23 19:13:23 +0530

    over riding of text in login removed

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -60,16 +60,21 @@ const LoginForm = () => {
             width: isSmallScreen ? '100%' : '50%',
             textAlign: 'center',
             height: '100%',
-            position: 'relative'
+            position: 'relative',
+            paddingTop: isSmallScreen ? '24px' : '32px', // Adjust padding-top for small screens
           }}
         >
-          <Box mt={4}>
+          <Box mt={isSmallScreen ? 2 : 4}>
             <img src="/logo.png" alt="Logo" style={{ maxWidth: '150px' }} /> {/* Replace with your logo */}
           </Box>
           <Typography
             variant="h6"
             gutterBottom
-            style={{ color: '#ECC290', fontSize: isSmallScreen ? '1rem' : '1.25rem' }}
+            style={{ 
+              color: '#ECC290', 
+              fontSize: isSmallScreen ? '1rem' : '1.25rem',
+              marginTop: isSmallScreen ? '8px' : '16px', // Adjust margin-top for small screens
+            }}
           >
             Welcome to the most trusted exclusive Muslim matrimony in Kerala
           </Typography>
